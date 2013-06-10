99cf0f6e33799d64a9dd124abe53d095864eee0d
Daniel Lo Nigro <daniel@dan.cx>
2013-06-10 17:48:06 +1000

    ASP.NET MVC 2 support

---
+++ src/RouteJs.Mvc2/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RouteJs.Mvc2")]
+[assembly: AssemblyDescription("ASP.NET MVC 2 extensions for RouteJs")]
+[assembly: ComVisible(false)]
+[assembly: Guid("988e08f1-fad9-486c-80fd-284a9596beb9")]
---
+++ src/RouteJs.Tests.Mvc2/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RouteJs.Tests.Mvc2")]
+[assembly: AssemblyDescription("Unit tests for ASP.NET MVC 2 extensions for RouteJs")]
+[assembly: ComVisible(false)]
+[assembly: Guid("953421bb-86a3-4f63-aa53-44e658b56433")]
